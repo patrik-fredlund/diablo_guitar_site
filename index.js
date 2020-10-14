cb3f5b3435510a3be7f92498e2fe7dff86c67814
patrik-fredlund <patrikfredlunds@gmail.com>
2020-10-14 20:28:04 +0200

    Cleaned up code

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-/* function validateForm() {
-  var x = document.forms["contactForm"]["name"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  } else {
-    Message.innerHTML = "";
-    return true;
-  }
-} */
+/* Contact-form-validation.
+Fadil hjälpte mig med denna. Vi satt i 4 timmar fram till kl01 sewn fick vi (han) det att funka. Lörorik session. Vilken hjälte han är. Jag är skyldig honom en lunch, minst.*/
 
 const submitBtn = document.querySelector(".submit");
 let nameInputEl = document.querySelector("#name");
@@ -17,25 +9,21 @@ let errorEl = document.querySelector(".error");
 var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 function validation() {
-  /* var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var message = document.getElementById("message").value;
-  var error_message = document.getElementById("error_message"); */
   errorEl.innerHTML = "";
   submitBtn.addEventListener("click", function (e) {
     e.preventDefault();
     errorEl.innerHTML = "";
     if (nameInputEl.value.length < 3) {
       errorEl.classList.remove("hide");
-      errorEl.innerHTML += "för kort<br>";
+      errorEl.innerHTML += "Too Short Name<br>";
     } else if (nameInputEl.value.length > 8) {
       errorEl.classList.remove("hide");
-      errorEl.innerHTML += "för lång<br>";
+      errorEl.innerHTML += "Too Long<br>";
     }
 
     if (!emailInputEl.value.match(mailformat)) {
       errorEl.classList.remove("hide");
-      errorEl.innerHTML += "Emailadress is wrong<br>";
+      errorEl.innerHTML += "Email adress is wrong<br>";
     }
 
     if (!msglInputEl.value) {
@@ -50,40 +38,3 @@ function validation() {
 }
 
 validation();
-
-/* var myForm = document.forms.myForm;
-var message = document.getElementById("message");
-
-myForm.onsubmit = function () {
-  if (myForm.name.value == "") {
-    message.innerHTML = "please enter a name";
-    return false;
-  } else {
-    message.innerHTML = "";
-    return true;
-  }
-}; */
-
-/* const name = document.getElementById("name");
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-const form = document.getElementById("form");
-const errorElement = document.getElementById("errror");
-
-form.addEventListener("submit", (e) => {
-  let message = [];
-  if (name.value === "" || name.value == nul) {
-    messages.push("Name is required");
-  }
-  if (message.length > 0) {
-    e.preventDefault();
-    errorElement.innerText = messages.join(",");
-  }
-});
- */
-
-/* const name = document.getElementById("name");
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-const contactForm = document.getElementById("form");
-const errorElement = document.getElementById("errror"); */
