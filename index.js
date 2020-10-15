a9c043e5655ca54ea4938062fa6f06ccd1fb70ea
patrik-fredlund <patrikfredlunds@gmail.com>
2020-10-15 17:15:38 +0200

    modified index.js contact.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 /* Contact-form-validation.
 Fadil hjälpte mig med denna. Vi satt i 4 timmar fram till kl01 sewn fick vi (han) det att funka. Lörorik session. Vilken hjälte han är. Jag är skyldig honom en lunch, minst.*/
 
+const myForm = document.querySelector("form");
 const submitBtn = document.querySelector(".submit");
 let nameInputEl = document.querySelector("#name");
 let emailInputEl = document.querySelector("#email");
 let msglInputEl = document.querySelector("#message");
 let errorEl = document.querySelector(".error");
+let msgSentEl = document.querySelector(".message-sent");
 var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 function validation() {
@@ -18,7 +20,7 @@ function validation() {
       errorEl.innerHTML += "Too Short Name<br>";
     } else if (nameInputEl.value.length > 8) {
       errorEl.classList.remove("hide");
-      errorEl.innerHTML += "Too Long<br>";
+      errorEl.innerHTML += "Too Long name<br>";
     }
 
     if (!emailInputEl.value.match(mailformat)) {
@@ -34,7 +36,27 @@ function validation() {
     if (errorEl.innerHTML == "") {
       errorEl.classList.add("hide");
     }
+    // Message sent kod snutt med en delay
+    if (errorEl.innerHTML == "") {
+      msgSentEl.classList.remove("hide");
+      msgSentEl.innerHTML = "Thank You.";
+      setTimeout(function () {
+        msgSentEl.classList.add("hide");
+        myForm.submit();
+      }, 3000);
+    }
   });
 }
 
 validation();
+
+function PlaySound(soundobj) {
+  var thissound = document.getElementById(soundobj);
+  thissound.play();
+}
+
+function StopSound(soundobj) {
+  var thissound = document.getElementById(soundobj);
+  thissound.pause();
+  thissound.currentTime = 0;
+}
