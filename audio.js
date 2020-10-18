bcfec8892cb68bd494b9283b85fbcff904227025
patrik-fredlund <patrikfredlunds@gmail.com>
2020-10-18 09:51:50 +0200

    modified audio.js

--- audio.js
+++ audio.js
@@ -1,4 +1,4 @@
-//Jag ville ha ljud på knapparna och när sidan laddas. Hittade en enkel lösning.
+//Jag ville ha ljud på knapparna och när sidan laddas. Skapade egna mp3 filer och Hittade en enkel lösning.
 
 
 
